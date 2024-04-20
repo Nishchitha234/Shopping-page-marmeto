1d3ad380e7a3cd14f30bfed65191927ee1def04f
Nishchitha234 <95737934+Nishchitha234@users.noreply.github.com>
2024-04-20 21:59:21 +0530

    Add files via upload
    New files upload

---
+++ products.js
@@ -0,0 +1,46 @@
+const products = [
+    {
+        "id": 1,
+        "name": "Red embroidery Design Saree",
+        "scratchPrice": "original price : 3̶0̶0̶0̶",
+        "price": 1200,
+        "rating": "(4 Star Rating)",
+        "offer": 35,
+        "image": "images/model5.jpg",
+        "description": "A stunning red saree adorned with intricate embroidery, it exudes elegance and charm. The rich crimson hue of the fabric captivates the eye, while the meticulous embroidery adds a touch of opulence. Delicate floral motifs or intricate patterns dance across the saree, crafted with precision and skill. The embroidery may be embellished with sequins, beads, or threadwork, enhancing its allure and making it a timeless piece of art. Whether worn for a special occasion or a cultural celebration, this red saree with exquisite embroidery is sure to make a statement and leave a lasting impression.",
+      },
+    
+    
+    {
+        "id": 2,
+        "name": "Red embroidery Design Saree",
+        "scratchPrice": "original price : 3̶0̶0̶0̶",
+        "price": 1200,
+        "rating": "(4 Star Rating)",
+        "offer": 35,
+        "image": "images/model6.jpg",
+        "description": "A stunning red saree adorned with intricate embroidery, it exudes elegance and charm. The rich crimson hue of the fabric captivates the eye, while the meticulous embroidery adds a touch of opulence. Delicate floral motifs or intricate patterns dance across the saree, crafted with precision and skill. The embroidery may be embellished with sequins, beads, or threadwork, enhancing its allure and making it a timeless piece of art. Whether worn for a special occasion or a cultural celebration, this red saree with exquisite embroidery is sure to make a statement and leave a lasting impression."
+    },
+    {
+        "id": 3,
+        "name": "Red embroidery Design Saree",
+        "scratchPrice": "original price : 3̶0̶0̶0̶",
+        "price": 1200,
+        "rating": "(4 Star Rating)",
+        "offer": 35,
+        "image": "images/model3.jpg",
+        "description": "A stunning red saree adorned with intricate embroidery, it exudes elegance and charm. The rich crimson hue of the fabric captivates the eye, while the meticulous embroidery adds a touch of opulence. Delicate floral motifs or intricate patterns dance across the saree, crafted with precision and skill. The embroidery may be embellished with sequins, beads, or threadwork, enhancing its allure and making it a timeless piece of art. Whether worn for a special occasion or a cultural celebration, this red saree with exquisite embroidery is sure to make a statement and leave a lasting impression."
+    },
+    {
+        "id": 4,
+        "name": "Red embroidery Design Saree",
+        "scratchPrice": "original price : 3̶0̶0̶0̶",
+        "price": 1200,
+        "rating": "(4 Star Rating)",
+        "offer": 35,
+        "image": "images/model4.jpg",
+        "description": "A stunning red saree adorned with intricate embroidery, it exudes elegance and charm. The rich crimson hue of the fabric captivates the eye, while the meticulous embroidery adds a touch of opulence. Delicate floral motifs or intricate patterns dance across the saree, crafted with precision and skill. The embroidery may be embellished with sequins, beads, or threadwork, enhancing its allure and making it a timeless piece of art. Whether worn for a special occasion or a cultural celebration, this red saree with exquisite embroidery is sure to make a statement and leave a lasting impression."
+    }
+
+];
+export default products;
